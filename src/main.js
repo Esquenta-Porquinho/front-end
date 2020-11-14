8ed48c09a50a104f76a8c33a281efefeba8be4a0
Kammradt <vinicius.kammradt1@gmail.com>
2020-11-14 18:39:20 -0300

    chore: :truck: moves App.vue to src folder

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import App from './App.vue';
+import App from '@/App.vue';
 import Clipboard from 'v-clipboard'
 import router from './router'
 import vuetify from './plugins/vuetify';
