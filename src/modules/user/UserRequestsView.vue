7e432fa15496b198e328f1bd6252a9106d0ac85e
Jean Moraes <jeanjms.1999@gmail.com>
2020-12-16 16:15:40 -0300

    Adding user Views and adding the texts in english and portuguese to that's views

---
+++ src/modules/user/UserRequestsView.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="background">
+    <ToolBar />
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col md="6" sm="8" xs="12">
+          <v-card>
+            <v-toolbar 
+              color="primary"
+              class="white--text" 
+              height="60px" 
+              width="100%" 
+              flat
+            >
+              <v-toolbar-title v-text="$t('views.user.requests.title')"/>
+              <v-spacer/>
+              <v-icon dark right v-text="'mdi-account-clock'"/>
+            </v-toolbar>
+              <v-simple-table>
+                <template>
+                  <tbody>
+                    <tr v-for="user in users" :key="user.id">
+                      <td><v-icon v-text="'mdi-account'"/>
+                      <td>{{ user.name }}</td>
+                      <td>{{ user.email }}</td>
+                      <td><v-btn x-small color="success" v-text="$t('buttons.activate')"/></td>
+                      <td><v-btn x-small color="error" v-text="$t('buttons.exclude')"/></td>
+                    </tr>
+                  </tbody>
+                </template>
+              </v-simple-table>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+    <FooterBar/>
+  </div>
+</template>
+
+<script>
+import ToolBar from "@/modules/common/ToolBar";
+import FooterBar from "@/modules/common/FooterBar";
+
+export default {
+  components: {ToolBar, FooterBar},
+  data:() =>({
+      users:[
+        {
+          id:1,
+          name:"Adamo Cornão",
+          email:"adamo@gmail.com",
+          role:"ADMIN"
+        },
+        {
+          id:2,
+          name:"Adamo Cornão",
+          email:"adamo@gmail.com",
+          role:"ADMIN"
+        },
+        {
+          id:3,
+          name:"Adamo Cornão",
+          email:"adamo@gmail.com",
+          role:"ADMIN"
+        },
+        {
+          id:4,
+          name:"Adamo Cornão",
+          email:"adamo@gmail.com",
+          role:"ADMIN"
+        },
+      ],
+  }),
+}
+</script>
+
+<style>
+  html {
+    overflow-y: auto !important;
+  }
+
+  .background {
+    background-image: url("../../assets/background.png");
+    height: 100%;
+  }
+</style>
---
+++ src/modules/user/UserView.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="background">
+    <ToolBar/>
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col md="4" sm="8" xs="12">
+          <v-card>
+            <v-toolbar 
+              color="primary"
+              class="white--text"
+              height="60px" 
+              width="100%" 
+              flat
+            >
+              <v-toolbar-title v-text="$t('views.user.profile.title')"/>
+              <v-spacer/>
+              <v-icon dark right v-text="'mdi-account'"/>
+            </v-toolbar>
+            <v-card-text>
+              <v-list>
+                <v-list-item-group  color="primary">
+                  <v-list-item>
+                    <v-list-item-icon>
+                      <v-icon v-text="'mdi-account'"/>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title v-text="user.name"/>
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-icon>
+                      <v-icon v-text="'mdi-email'"/>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title v-text="user.email"/>
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-icon>
+                      <v-icon v-text="'mdi-account-box'"/>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title v-text="user.role" />
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+              <v-card-actions>
+                <v-btn
+                  block
+                  color="secondary"
+                  v-text="$t('buttons.update')"
+                  :to="'/user/update'"
+                />
+              </v-card-actions>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+    <FooterBar/>
+  </div>
+</template>
+
+<script>
+import ToolBar from "@/modules/common/ToolBar";
+import FooterBar from "@/modules/common/FooterBar";
+
+export default {
+  components: {ToolBar, FooterBar},
+  data:()=>({
+    user:{
+        id:1,
+        name:"Adamo Cornão",
+        email:"adamo@gmail.com",
+        role:"ADMIN"
+    },
+  }),
+  methods: {
+
+  },
+}
+</script>
+
+<style scoped>
+  .background {
+    background-image: url("../../assets/background1.png");
+    height: 100%;
+  }
+</style>
