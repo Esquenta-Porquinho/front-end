a5fff1f3de26e69f318bae173e189eeea48fd32f
Jean Moraes <jeanjms.1999@gmail.com>
2021-03-28 14:42:34 -0300

    splitting the router to make each module have it's own router file

--- src/router/index.js
+++ src/router/index.js
@@ -1,71 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../modules/home/HomeView'
-import SettingsView from '../modules/home/SettingsView';
-import LoginView from '../modules/auth/Login';
-import RegisterView from '../modules/auth/Register';
-import UserView from  '../modules/user/UserView';
-import UserUpdateView from '../modules/user/UserUpdateView';
-import UserLogsView from '../modules/user/UserLogsView';
-import UserUpdateRoleView from '../modules/user/UserUpdateRoleView';
-import UserRequestsView from '../modules/user/UserRequestsView';
-import UserAllView from '../modules/user/UserAllView';
+import { authRoutes } from '@/modules/auth/auth-routes';
+import { userRoutes } from '@/modules/user/user-routes';
+import { matrixRoutes } from '@/modules/matrix/matrix-routes';
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'HomeView',
-    component: HomeView,
-    children: [
-      {
-        path: 'settings',
-        name: 'SettingsView',
-        component: SettingsView
-      }
-    ]
-  },
-  {
-    path: '/login',
-    name: 'LoginView',
-    component: LoginView
-  },
-  {
-    path: '/register',
-    name: 'RegisterView',
-    component: RegisterView
-  },
-  {
-    path: '/user',
-    name: 'UserView',
-    component: UserView
-  },
-  {
-    path: '/user/update',
-    name: 'UserUpdateView',
-    component: UserUpdateView
-  },
-  {
-    path: '/user/logs',
-    name: 'UserLogsView',
-    component: UserLogsView
-  },
-  {
-    path: '/user/role',
-    name: 'UserUpdateRoleView',
-    component: UserUpdateRoleView
-  },
-  {
-    path: '/users/requests',
-    name: 'UserRequestsView',
-    component: UserRequestsView
-  },
-  {
-    path: '/users',
-    name: 'UserAllView',
-    component: UserAllView
-  },
+  ...authRoutes,
+  ...userRoutes,
+  ...matrixRoutes,
 ]
 
 const router = new VueRouter({
