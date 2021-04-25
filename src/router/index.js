7429b4e9f7d22be3e8a7892ff2c11c5bec49e3d3
Jean de Junio Moraes Silva <jeanjms.1999@gmail.com>
2021-04-25 12:13:13 -0300

    feat: add charts routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,17 @@ import { authRoutes } from "@/modules/auth/auth-routes";
 import { userRoutes } from "@/modules/user/user-routes";
 import { matrixRoutes } from "@/modules/matrix/matrix-routes";
 import { boxRoutes } from "../modules/box/box-routes";
+import { chartsRoutes } from "../modules/charts/charts-routes";
 
 Vue.use(VueRouter);
 
-const routes = [...authRoutes, ...userRoutes, ...matrixRoutes, ...boxRoutes];
+const routes = [
+  ...authRoutes,
+  ...userRoutes,
+  ...matrixRoutes,
+  ...boxRoutes,
+  ...chartsRoutes,
+];
 
 const router = new VueRouter({
   mode: "history",
